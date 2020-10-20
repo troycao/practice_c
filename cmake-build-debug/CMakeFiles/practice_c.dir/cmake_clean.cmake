1e85a7f53c206f521404ff669f73d392a5c47a58
caoqiang <caoqiang@aikucun.com>
2020-10-20 15:05:19 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/practice_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice_c.dir/main.c.obj"
+  "practice_c.exe"
+  "practice_c.exe.manifest"
+  "practice_c.lib"
+  "practice_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/practice_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
